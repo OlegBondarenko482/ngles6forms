adb184d0f0c36a8135ac0e17570608f564e1baad
Oleg <gmoleg82@gmail.com>
2020-05-02 12:59:21 +0300

    with dz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {FirstComponent} from './first/first.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'untitled1';
+  @ViewChild('target', {static: false})
+  target;
+  @ViewChild(FirstComponent, {static: false})
+  firstcompn: FirstComponent;
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.target.nativeElement.innerText);
+    console.log(this.firstcompn);
+    this.firstcompn.hello();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CanActivateService} from './services/guards/can-activate.service';
+import { HelloComponent } from './hello/hello.component';
+
+const routes: Routes = [
+  {path: '', component: HelloComponent},
+  {path: 'first', component: FirstComponent, canActivate: [CanActivateService], canActivateChild: [], canDeactivate: [CanActivateService]},
+  {path: 'second', component: SecondComponent},
+  // {path: 'second', component: SecondComponent, outlet: 'xxx'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
